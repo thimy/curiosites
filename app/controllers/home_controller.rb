0853ab893f29b369cd23dc5b5f6a4bc44a81f689
thimy <thimy.nya@gmail.com>
2016-12-05 20:25:59 +0000

    display class attributes on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @name = 'Juna'
+    @race = 'Shiba Inu'
+    @description = 'Patronne'
+    
+    @stuff = ['basket', 'kong', 'dentastix', 'bone']
   end
 end
